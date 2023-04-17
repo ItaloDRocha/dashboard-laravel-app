bfdc64008c58d7a831cab2be00902260f95adbd5
italoDRocha <italorocha305@gmail.com>
2023-04-17 18:44:27 -0300

    template admin/migrations

---
+++ resources/views/admindashboard.blade.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Enlink - Admin Dashboard Template</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/images/logo/favicon.png">
+
+    <link rel="stylesheet" href="{{asset('assets/css/app.min.css')}}">
+    
+    {{-- <script src="{{ asset('build/assets/app.d225c007.js') }}"></script> --}}
+
+    <!-- page css -->
+
+    <!-- Core css -->
+    {{-- <link href="../css/app.min.css" rel="stylesheet"> --}}
+    {{-- @vite(['resources/css/app.min.css']) --}}
+    
+    {{-- @vite(['resources/css/app.min.css', 'resources/js/vendors.min.js', 'resources/js/pages/chartjs.js', 'resources/js/pages/dashboard-default.js', 'resources/js/app.min.js']) --}}
+</head>
+
+<body>
+    <div class="app">
+        <div class="layout">
+            <!-- Header START -->
+            <div class="header">
+                <div class="logo logo-dark">
+                    <a href="{{route('admindashboard')}}">
+                        <img src="assets/images/logo/logo.png" alt="Logo">
+                        <img class="logo-fold" src="assets/images/logo/logo-fold.png" alt="Logo">
+                    </a>
+                </div>
+                <div class="logo logo-white">
+                    <a href="{{route('admindashboard')}}">
+                        <img src="assets/images/logo/logo-white.png" alt="Logo">
+                        <img class="logo-fold" src="assets/images/logo/logo-fold-white.png" alt="Logo">
+                    </a>
+                </div>
+                <div class="nav-wrap">
+                    <ul class="nav-left">
+                        <li class="desktop-toggle">
+                            <a href="javascript:void(0);">
+                                <i class="anticon"></i>
+                            </a>
+                        </li>
+                        <li class="mobile-toggle">
+                            <a href="javascript:void(0);">
+                                <i class="anticon"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="javascript:void(0);" data-toggle="modal" data-target="#search-drawer">
+                                <i class="anticon anticon-search"></i>
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="nav-right">
+                        <li class="dropdown dropdown-animated scale-left">
+                            <div class="pointer" data-toggle="dropdown">
+                                <div class="avatar avatar-image  m-h-10 m-r-15">
+                                    <img src="assets/images/avatars/thumb-3.jpg"  alt="">
+                                </div>
+                            </div>
+                            <div class="p-b-15 p-t-20 dropdown-menu pop-profile">
+                                <div class="p-h-20 p-b-15 m-b-10 border-bottom">
+                                    <div class="d-flex m-r-50">
+                                        <div class="avatar avatar-lg avatar-image">
+                                            <img src="assets/images/avatars/thumb-3.jpg" alt="">
+                                        </div>
+                                        <div class="m-l-10">
+                                            <p class="m-b-0 text-dark font-weight-semibold">Marshall Nichols</p>
+                                            <p class="m-b-0 opacity-07">UI/UX Desinger</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="javascript:void(0);" class="dropdown-item d-block p-h-15 p-v-10">
+                                    <div class="d-flex align-items-center justify-content-between">
+                                        <div>
+                                            <i class="anticon opacity-04 font-size-16 anticon-user"></i>
+                                            <span class="m-l-10">Edit Profile</span>
+                                        </div>
+                                        <i class="anticon font-size-10 anticon-right"></i>
+                                    </div>
+                                </a>
+                                <a href="javascript:void(0);" class="dropdown-item d-block p-h-15 p-v-10">
+                                    <div class="d-flex align-items-center justify-content-between">
+                                        <div>
+                                            <i class="anticon opacity-04 font-size-16 anticon-lock"></i>
+                                            <span class="m-l-10">Account Setting</span>
+                                        </div>
+                                        <i class="anticon font-size-10 anticon-right"></i>
+                                    </div>
+                                </a>
+                                <a href="javascript:void(0);" class="dropdown-item d-block p-h-15 p-v-10">
+                                    <div class="d-flex align-items-center justify-content-between">
+                                        <div>
+                                            <i class="anticon opacity-04 font-size-16 anticon-logout"></i>
+                                            <span class="m-l-10">Logout</span>
+                                        </div>
+                                        <i class="anticon font-size-10 anticon-right"></i>
+                                    </div>
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>    
+            <!-- Header END -->
+
+            <!-- Side Nav START -->
+            <div class="side-nav">
+                <div class="side-nav-inner">
+                    <ul class="side-nav-menu scrollable">
+                        <li class="nav-item dropdown open">
+                            <a class="dropdown-toggle" href="javascript:void(0);">
+                                <span class="icon-holder">
+                                    <i class="anticon anticon-dashboard"></i>
+                                </span>
+                                <span class="title">Dashboard</span>
+                                <span class="arrow">
+                                    <i class="arrow-icon"></i>
+                                </span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li class="active">
+                                    <a href="{{route('admindashboard')}}">Default</a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <!-- Side Nav END -->
+
+            <!-- Page Container START -->
+            <div class="page-container">
+                
+
+                <!-- Content Wrapper START -->
+                <div class="main-content">
+                    <div class="row">
+                        <div class="col-md-6 col-lg-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="media align-items-center">
+                                        <div class="avatar avatar-icon avatar-lg avatar-blue">
+                                            <i class="anticon anticon-dollar"></i>
+                                        </div>
+                                        <div class="m-l-15">
+                                            <h2 class="m-b-0">$23,523</h2>
+                                            <p class="m-b-0 text-muted">Profit</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="media align-items-center">
+                                        <div class="avatar avatar-icon avatar-lg avatar-cyan">
+                                            <i class="anticon anticon-line-chart"></i>
+                                        </div>
+                                        <div class="m-l-15">
+                                            <h2 class="m-b-0">+ 17.21%</h2>
+                                            <p class="m-b-0 text-muted">Growth</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="media align-items-center">
+                                        <div class="avatar avatar-icon avatar-lg avatar-gold">
+                                            <i class="anticon anticon-profile"></i>
+                                        </div>
+                                        <div class="m-l-15">
+                                            <h2 class="m-b-0">3,685</h2>
+                                            <p class="m-b-0 text-muted">Orders</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="media align-items-center">
+                                        <div class="avatar avatar-icon avatar-lg avatar-purple">
+                                            <i class="anticon anticon-user"></i>
+                                        </div>
+                                        <div class="m-l-15">
+                                            <h2 class="m-b-0">1,832</h2>
+                                            <p class="m-b-0 text-muted">Customers</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Page Container END -->
+            <!-- Quick View END -->
+        </div>
+    </div>
+
+    <script src="{{ asset('assets/js/vendors.min.js')}}"></script>
+
+    <script src="{{ asset('assets/js/pages/chartjs.js')}}"></script>
+    <script src="{{ asset('assets/js/pages/dashboard-default.js')}}"></script>
+
+    <script src="{{ asset('assets/js/app.min.js')}}"></script>
+
+    
+    {{-- <!-- Core Vendors JS -->
+        
+    <script src="../js/vendors.min.js"></script>
+
+    <!-- page js -->
+    <script src="../vendors/chartjs/Chart.min.js"></script>
+    <script src="../js/pages/dashboard-default.js"></script>
+
+    <!-- Core JS -->
+    <script src="../js/app.min.js"></script> --}}
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::view('/admindashboard', 'admindashboard')->name('admindashboard');
+
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
